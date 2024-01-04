3340d73c3f0c538e4b62cd9be6e2eeeda5c915b8
mohamed hamidani <mohamidani1@gmail.com>
2024-01-04 06:17:44 +0300

    design is suck i think xd

--- layouts/MainLay.vue
+++ layouts/MainLay.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
+    <div class="min-w-screen min-h-screen">
         <nav class="shadow-md h-14 w-full flex justify-center items-center">
             <h3 class="w-full bg-gradient-to-r from-50% from-red-400 to-yellow-400 text-4xl text-center bg-clip-text text-transparent h-full mt-2 font-semibold cursor-pointer">Fateer Tracking </h3>
         </nav>
-        <slot />
+        <div class="w-full h-full bg-white/3">
+            <slot />
+        </div>
     </div>
 </template>
 
--- pages/meat.vue
+++ pages/meat.vue
@@ -1,6 +1,31 @@
 <template>
     <MainLay>
-        div
+        <div class="w-full flex flex-col items-center">
+            <div class="h-16 w-full  my-5 flex justify-around items-center">
+                <select class="outline-none w-[200px] h-9 rounded text-2xl capitalize bg-gray-100" name="meals" id="">
+                    <option value="">meat</option>
+                    <option value="">meat</option>
+                    <option value="">meat</option>
+                </select>
+                <div class="flex flex-col justify-center items-center">
+                    <input placeholder="Quantity" type="text" class="text-2xl text-black w-[200px] h-9 rounded outline-none pl-2 bg-gray-100">
+                </div>
+                <div class="flex flex-col justify-center items-center">
+                    <input placeholder="price" type="text" class=" capitalize text-2xl text-black w-[200px] h-9 rounded outline-none pl-2 bg-gray-100">
+                </div>
+            </div>
+            <button class="w-[100px] text-xl capitalize text-white h-9 bg-red-200 bg-gradient-to-r from-50% hover:opacity-80 duration-500 from-red-400 to-yellow-400 rounded ">submit</button>
+        </div>
+        <div class="mt-5">
+            <table>
+                <tr class="bg-gradient-to-r from-50% from-red-400 to-yellow-400 text-white text-2xl">
+                  <th class="min-w-[250px] h-10 capitalize border-x-2 text-center border-black w-[400px]">type</th>
+                  <th class="min-w-[250px] h-12 capitalize border-x-2 text-center border-black w-[400px]">Qnt</th>
+                  <th class="min-w-[250px] h-10 capitalize border-x-2 text-center border-black w-[400px]">Price</th>
+                  <th class="min-w-[250px] w-[400px] h-10 capitalize border-x-2 text-center border-black">total</th>
+                </tr>
+              </table>
+        </div>
     </MainLay>
 </template>
 
